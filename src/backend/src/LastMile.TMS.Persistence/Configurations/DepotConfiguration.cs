19e07ae077ac248ce48e391f0143072cfc5d1313
Ivan Shatalov <shatalov17super@gmail.com>
2026-03-15 21:18:49 +0300

    fix: change Depot.Address from owned type to FK relationship

--- src/backend/src/LastMile.TMS.Persistence/Configurations/DepotConfiguration.cs
+++ src/backend/src/LastMile.TMS.Persistence/Configurations/DepotConfiguration.cs
@@ -19,21 +19,13 @@ public class DepotConfiguration : IEntityTypeConfiguration<Depot>
         builder.Property(d => d.IsActive)
             .HasDefaultValue(true);
 
-        builder.OwnsOne(d => d.Address, a =>
-        {
-            a.Property(p => p.Street1).IsRequired().HasMaxLength(200).HasColumnName("Street1");
-            a.Property(p => p.Street2).HasMaxLength(200).HasColumnName("Street2");
-            a.Property(p => p.City).IsRequired().HasMaxLength(100).HasColumnName("City");
-            a.Property(p => p.State).IsRequired().HasMaxLength(100).HasColumnName("State");
-            a.Property(p => p.PostalCode).IsRequired().HasMaxLength(20).HasColumnName("PostalCode");
-            a.Property(p => p.CountryCode).IsRequired().HasMaxLength(2).HasColumnName("CountryCode");
-            a.Property(p => p.IsResidential).HasColumnName("IsResidential");
-            a.Property(p => p.ContactName).HasMaxLength(150).HasColumnName("ContactName");
-            a.Property(p => p.CompanyName).HasMaxLength(200).HasColumnName("CompanyName");
-            a.Property(p => p.Phone).HasMaxLength(20).HasColumnName("Phone");
-            a.Property(p => p.Email).HasMaxLength(254).HasColumnName("Email");
-            a.Property(p => p.GeoLocation).HasColumnType("geometry (point)").HasColumnName("GeoLocation");
-        });
+        builder.HasOne(d => d.Address)
+            .WithMany()
+            .HasForeignKey(d => d.AddressId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(d => d.AddressId)
+            .HasColumnName("AddressId");
 
         builder.OwnsOne(d => d.OperatingHours, oh =>
         {
